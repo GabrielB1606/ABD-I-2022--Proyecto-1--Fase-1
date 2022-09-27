782aa8dd6defe5ffafc29c4b2b773a6ed62fbee7
Gabriel Belisario <gabrielbelisariof@gmail.com>
2022-09-27 16:58:09 -0400

    create sql first approach
    Co-authored-by: Gabriel Carrizo <gabrielLiendo@users.noreply.github.com>

---
+++ create.sql
@@ -0,0 +1,82 @@
+-- tablespaces and what not
+create tablespace TS_ABD_P1_GBGC datafile 'DF_ABD_P1_GBGC.dbf' size 10m;
+
+-- create entity tables
+
+create table usuario(
+    correo          varchar2(32),
+    nombre          varchar2(32)    NOT NULL,
+    apellido        varchar2(32)    NOT NULL,
+    clave           varchar2(32)    NOT NULL,
+    direccion       varchar2(128),
+    telefono        varchar2(16),
+    es_expositor    boolean         NOT NULL,
+    PRIMARY KEY (correo)
+) tablespace TS_ABD_P1_GBGC;
+
+create table universidad(
+    nombre          varchar2(32),
+    descripcion     varchar2(256),
+    fecha_creacion  date,
+    acronimo        varchar2(32),
+    PRIMARY KEY (nombre)
+) tablespace TS_ABD_P1_GBGC;
+
+create table empresa(
+    nombre          varchar2(32),   
+    direccion       varchar2(128),
+    telefono        varchar2(16),
+    es_privada      boolean
+    PRIMARY KEY (nombre)
+) tablespace TS_ABD_P1_GBGC;
+
+create table evento(
+    id                  number,
+    nombre_universidad  varchar2(64),
+    nombre              varchar2(32),
+    fecha_inicio        date,
+    fecha_fin           date,
+    area                varchar2(64),
+    PRIMARY KEY(id),
+    FOREIGN KEY(nombre_universidad) REFERENCES universidad(nombre)
+) tablespace TS_ABD_P1_GBGC;
+
+-- create relation tables
+
+create table pertenece(
+    nombre_universidad  varchar2(64),
+    correo_usuario      varchar2(32),
+    fecha_registro      date,
+    PRIMARY KEY (nombre_universidad, correo_usuario),
+    FOREIGN KEY (nombre_universidad) REFERENCES universidad(nombre),
+    FOREIGN KEY (correo_usuario) REFERENCES usuario(correo)
+) tablespace TS_ABD_P1_GBGC;
+
+create table participa(
+    id_evento               number,
+    correo_usuario          varchar2(32),
+    PRIMARY KEY (id_evento, correo_usuario),
+    FOREIGN KEY (id_evento) REFERENCES evento(id),
+    FOREIGN KEY (correo_usuario) REFERENCES usuario(correo_usuario)
+) tablespace TS_ABD_P1_GBGC;
+
+create table dicta(
+    id_evento               number,
+    correo_usuario          varchar2(32),
+    nombre_charla           varchar2(32),
+    valoracion_charla       number,
+    valoracion_contenido    number,
+    PRIMARY KEY (id_evento, correo_usuario),
+    FOREIGN KEY (id_evento) REFERENCES evento(id),
+    FOREIGN KEY (correo_usuario) REFERENCES usuario(correo_usuario)
+) tablespace TS_ABD_P1_GBGC;
+
+create table patrocina(
+    id_evento               number,
+    nombre_empresa          varchar2(32),
+    aporte                  number,
+    fecha_aporte            date,
+    PRIMARY KEY (id_evento, nombre_empresa),
+    FOREIGN KEY (id_evento) REFERENCES evento(id),
+    FOREIGN KEY (nombre_empresa) REFERENCES empresa(nombre)
+) tablespace TS_ABD_P1_GBGC;
