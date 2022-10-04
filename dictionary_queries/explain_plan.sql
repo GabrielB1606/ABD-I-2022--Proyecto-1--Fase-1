0bafec0d2e8dfe0bbda845af11d04f6024810ec6
Gabriel Belisario <gabrielbelisariof@gmail.com>
2022-10-03 22:22:01 -0400

    explain plan bullshit

--- dictionary_queries/explain_plan.sql
+++ dictionary_queries/explain_plan.sql
@@ -1,13 +1,22 @@
-explain plan for
-    select id, acronimo
+-- SET LINESIZE 165
+-- SET PAGESIZE 0
+set autotrace on statistics
+-- explain plan for
+--  set autotrace traceonly explain
+    select 
+        id, acronimo 
         from universidad
         where acronimo = 'UCV';
 
-select * from table(dbms_xplan.display);
+-- SELECT * FROM TABLE(DBMS_XPLAN.DISPLAY_CURSOR(FORMAT=>'+ALLSTATS LAST'));
 
-explain plan for
-    select id, acronimo
+-- explain plan for
+--  set autotrace traceonly explain
+    select 
+        id, acronimo
         from universidad
         where id = 3;
+        
+set autotrace off statistics
 
-select * from table(dbms_xplan.display);
+-- SELECT * FROM TABLE(DBMS_XPLAN.DISPLAY_CURSOR(FORMAT=>'+ALLSTATS LAST'));
