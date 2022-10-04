1576a0891ae5dd137cd0cc8ed8eb1c88a50ba6dc
Gabriel Belisario <gabrielbelisariof@gmail.com>
2022-10-04 18:00:56 -0400

    formatting...

--- dictionary_queries/explain_plan.sql
+++ dictionary_queries/explain_plan.sql
@@ -1,13 +1,14 @@
 -- SET LINESIZE 165
 -- SET PAGESIZE 0
 set autotrace on statistics
-
+-- Primer Select: no se usa ningún Index
 select 
     id,
     acronimo 
 from universidad
 where acronimo = 'UCV';
 
+-- Segundo Select: se usa el Index de ID
 select 
     id,
     acronimo
